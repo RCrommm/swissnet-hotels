071cac0e8c513ee1ce4b4231b892b55abce82bf8
Romeo Carpentier <romeocarpentier@Romeos-MacBook-Pro.local>
2026-04-27 13:50:40 +0800

    Fix RoomTypeSchema TypeScript types

--- components/RoomTypeSchema.tsx
+++ components/RoomTypeSchema.tsx
@@ -1,190 +1,121 @@
-// components/RoomTypeSchema.jsx
-// Injects nested JSON-LD HotelRoom schema markup for AI crawler visibility
-// Usage: <RoomTypeSchema hotel={hotel} roomTypes={roomTypes} />
+type RoomType = {
+  id: string
+  is_available: boolean
+  name: string
+  description?: string
+  max_occupancy?: number
+  bed_type?: string
+  size_sqm?: number
+  amenities?: string[]
+  base_rate_chf?: number
+  view?: string
+  type_category?: string
+  short_description?: string
+  ai_description?: string
+  thumbnail_url?: string
+  images?: Array<string | { url: string }>
+  booking_url?: string
+  price_per_night?: number
+  price_currency?: string
+  price_weekend?: number
+  price_peak?: number
+  min_stay_nights?: number
+  floor_level?: string
+  highlights?: string[]
+  sort_order?: number
+}
 
-export default function RoomTypeSchema({ hotel, roomTypes }) {
-  if (!hotel || !roomTypes?.length) return null;
+type Hotel = {
+  id: string
+  slug?: string
+  name: string
+  description?: string
+  address?: string
+  city?: string
+  country?: string
+  latitude?: number
+  longitude?: number
+  star_rating?: number
+  phone?: string
+  email?: string
+  hero_image?: string
+  thumbnail_url?: string
+  amenities?: string[]
+  price_range?: string
+}
 
-  // Build the full Hotel + HotelRoom nested schema
+export default function RoomTypeSchema({ hotel, roomTypes }: { hotel: Hotel; roomTypes: RoomType[] }) {
+  if (!hotel || !roomTypes?.length) return null
+
   const schema = {
-    "@context": "https://schema.org",
-    "@type": "Hotel",
-    "@id": `https://swissnethotels.com/hotels/${hotel.slug}#hotel`,
-    "name": hotel.name,
-    "description": hotel.description || hotel.short_description,
-    "url": `https://swissnethotels.com/hotels/${hotel.slug}`,
-    "telephone": hotel.phone || undefined,
-    "email": hotel.email || undefined,
-    "starRating": hotel.star_rating ? {
-      "@type": "Rating",
-      "ratingValue": hotel.star_rating
+    '@context': 'https://schema.org',
+    '@type': 'Hotel',
+    '@id': `https://swissnethotels.com/hotels/${hotel.slug || hotel.id}#hotel`,
+    name: hotel.name,
+    description: hotel.description,
+    address: hotel.address ? {
+      '@type': 'PostalAddress',
+      streetAddress: hotel.address,
+      addressLocality: hotel.city,
+      addressCountry: hotel.country || 'CH',
     } : undefined,
-    "address": hotel.address ? {
-      "@type": "PostalAddress",
-      "streetAddress": hotel.address,
-      "addressLocality": hotel.city,
-      "addressCountry": hotel.country || "CH"
+    geo: (hotel.latitude && hotel.longitude) ? {
+      '@type': 'GeoCoordinates',
+      latitude: hotel.latitude,
+      longitude: hotel.longitude,
     } : undefined,
-    "geo": (hotel.latitude && hotel.longitude) ? {
-      "@type": "GeoCoordinates",
-      "latitude": hotel.latitude,
-      "longitude": hotel.longitude
-    } : undefined,
-    "image": hotel.hero_image || hotel.thumbnail_url || undefined,
-    "priceRange": hotel.price_range || "$$$$",
-    "currenciesAccepted": "CHF, EUR, USD",
-    "paymentAccepted": "Credit Card, Cash",
-    "amenityFeature": (hotel.amenities || []).map(amenity => ({
-      "@type": "LocationFeatureSpecification",
-      "name": amenity,
-      "value": true
+    starRating: hotel.star_rating ? { '@type': 'Rating', ratingValue: hotel.star_rating } : undefined,
+    amenityFeature: (hotel.amenities || []).map((a: string) => ({
+      '@type': 'LocationFeatureSpecification',
+      name: a,
+      value: true,
     })),
-    // Nested containsPlace for each room type
-    "containsPlace": roomTypes
-      .filter(rt => rt.is_available !== false)
-      .map(rt => buildRoomSchema(rt, hotel))
-  };
+    containsPlace: roomTypes.filter(rt => rt.is_available).map(rt => ({
+      '@type': 'HotelRoom',
+      '@id': `https://swissnethotels.com/hotels/${hotel.slug || hotel.id}#room-${rt.id}`,
+      name: rt.name,
+      description: rt.ai_description || rt.description || rt.short_description,
+      occupancy: {
+        '@type': 'QuantitativeValue',
+        minValue: 1,
+        maxValue: rt.max_occupancy || 2,
+      },
+      bed: rt.bed_type ? {
+        '@type': 'BedDetails',
+        typeOfBed: rt.bed_type,
+        numberOfBeds: 1,
+      } : undefined,
+      floorSize: rt.size_sqm ? {
+        '@type': 'QuantitativeValue',
+        value: rt.size_sqm,
+        unitCode: 'MTK',
+      } : undefined,
+      amenityFeature: (rt.amenities || []).map((a: string) => ({
+        '@type': 'LocationFeatureSpecification',
+        name: a,
+        value: true,
+      })),
+      offers: (rt.base_rate_chf || rt.price_per_night) ? {
+        '@type': 'Offer',
+        price: rt.base_rate_chf || rt.price_per_night,
+        priceCurrency: rt.price_currency || 'CHF',
+        url: rt.booking_url || `https://swissnethotels.com/hotels/${hotel.slug || hotel.id}`,
+        availability: 'https://schema.org/InStock',
+      } : undefined,
+      additionalProperty: rt.view ? [{
+        '@type': 'PropertyValue',
+        name: 'View',
+        value: rt.view,
+      }] : undefined,
+    })),
+  }
 
-  // Remove undefined values for clean output
-  const cleanSchema = JSON.parse(JSON.stringify(schema, (key, val) => 
-    val === undefined ? undefined : val
-  ));
+  const clean = JSON.parse(JSON.stringify(schema, (_, v) => v === undefined ? undefined : v))
 
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(cleanSchema, null, 0) }}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(clean) }}
     />
-  );
-}
-
-function buildRoomSchema(rt, hotel) {
-  const amenityFeatures = (rt.amenities || []).map(a => ({
-    "@type": "LocationFeatureSpecification",
-    "name": a,
-    "value": true
-  }));
-
-  // Build offer block
-  const offers = rt.price_per_night ? {
-    "@type": "Offer",
-    "price": rt.price_per_night,
-    "priceCurrency": rt.price_currency || "CHF",
-    "priceSpecification": buildPriceSpec(rt),
-    "url": rt.booking_url || `https://swissnethotels.com/hotels/${hotel.slug}#book`,
-    "availability": "https://schema.org/InStock",
-    "validFrom": new Date().toISOString().split('T')[0],
-    "seller": {
-      "@type": "Hotel",
-      "name": hotel.name
-    }
-  } : undefined;
-
-  const images = [];
-  if (rt.thumbnail_url) images.push(rt.thumbnail_url);
-  if (rt.images?.length) {
-    rt.images.forEach(img => {
-      const url = typeof img === 'string' ? img : img.url;
-      if (url && !images.includes(url)) images.push(url);
-    });
-  }
-
-  return {
-    "@type": "HotelRoom",
-    "@id": `https://swissnethotels.com/hotels/${hotel.slug}#room-${rt.id}`,
-    "name": rt.schema_name || rt.name,
-    "description": rt.ai_description || rt.description || rt.short_description,
-    "url": rt.booking_url || `https://swissnethotels.com/hotels/${hotel.slug}`,
-    "image": images.length === 1 ? images[0] : images.length > 1 ? images : undefined,
-    "occupancy": {
-      "@type": "QuantitativeValue",
-      "minValue": 1,
-      "maxValue": rt.max_occupancy || 2
-    },
-    "bed": rt.bed_type ? {
-      "@type": "BedDetails",
-      "typeOfBed": rt.bed_type,
-      "numberOfBeds": rt.bed_count || 1
-    } : undefined,
-    "floorSize": rt.size_sqm ? {
-      "@type": "QuantitativeValue",
-      "value": rt.size_sqm,
-      "unitCode": "MTK"
-    } : undefined,
-    "amenityFeature": amenityFeatures.length ? amenityFeatures : undefined,
-    "offers": offers,
-    // Additional descriptive properties
-    "additionalProperty": buildAdditionalProps(rt)
-  };
-}
-
-function buildPriceSpec(rt) {
-  const specs = [{
-    "@type": "UnitPriceSpecification",
-    "price": rt.price_per_night,
-    "priceCurrency": rt.price_currency || "CHF",
-    "unitText": "NIGHT",
-    "priceType": "https://schema.org/ListPrice"
-  }];
-
-  if (rt.price_weekend) {
-    specs.push({
-      "@type": "UnitPriceSpecification",
-      "price": rt.price_weekend,
-      "priceCurrency": rt.price_currency || "CHF",
-      "unitText": "NIGHT",
-      "name": "Weekend Rate",
-      "priceType": "https://schema.org/SalePrice"
-    });
-  }
-
-  if (rt.price_peak) {
-    specs.push({
-      "@type": "UnitPriceSpecification",
-      "price": rt.price_peak,
-      "priceCurrency": rt.price_currency || "CHF",
-      "unitText": "NIGHT",
-      "name": "Peak Season Rate"
-    });
-  }
-
-  return specs.length === 1 ? specs[0] : specs;
-}
-
-function buildAdditionalProps(rt) {
-  const props = [];
-
-  if (rt.view_type) {
-    props.push({
-      "@type": "PropertyValue",
-      "name": "View",
-      "value": rt.view_type
-    });
-  }
-
-  if (rt.floor_level) {
-    props.push({
-      "@type": "PropertyValue",
-      "name": "Floor",
-      "value": rt.floor_level
-    });
-  }
-
-  if (rt.type_category) {
-    props.push({
-      "@type": "PropertyValue",
-      "name": "RoomCategory",
-      "value": rt.type_category
-    });
-  }
-
-  if (rt.min_stay_nights && rt.min_stay_nights > 1) {
-    props.push({
-      "@type": "PropertyValue",
-      "name": "MinimumStay",
-      "value": `${rt.min_stay_nights} nights`
-    });
-  }
-
-  return props.length ? props : undefined;
+  )
 }
